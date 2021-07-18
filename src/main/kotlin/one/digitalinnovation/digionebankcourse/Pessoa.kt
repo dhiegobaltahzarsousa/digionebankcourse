35fe2f22b9eedfebb81b107611013e4d73940b56
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2021-07-18 20:01:43 -0300

    no kotlin da para criar inner class

--- src/main/kotlin/one/digitalinnovation/digionebankcourse/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebankcourse/Pessoa.kt
@@ -3,12 +3,22 @@ package one.digitalinnovation.digionebankcourse
 class Pessoa {
     var nome: String? = null
     var cpf: String? = null
+
+    //Consigo fazer inner class tbm
+    inner class Endereco{
+        var endereco: String? = null
+    }
 }
 
+
 fun main(){
-    val pessoa = Pessoa()
+    var pessoa = Pessoa()
     pessoa.nome = "Dhiego"
     pessoa.cpf="123464"
+    var endereco = pessoa.Endereco()
+    endereco.endereco = "Rua do Teste"
+
     println(pessoa.nome)
     println(pessoa.cpf)
+    println(endereco.endereco)
 }
