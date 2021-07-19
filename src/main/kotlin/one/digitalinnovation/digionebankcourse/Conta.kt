990b4646724d5c9dbb5ade183b012d30332dffa2
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2021-07-19 00:08:41 -0300

    Refactorated =>  Class Pessoa and Created => Class Conta

---
+++ src/main/kotlin/one/digitalinnovation/digionebankcourse/Conta.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebankcourse
+
+import java.math.BigDecimal
+
+class Conta(
+    var agencia: String?,
+    var conta: String?,
+    var saldo: BigDecimal
+
+) {
+    fun sacar(valor:BigDecimal){
+        //ToDo
+    }
+
+    fun depositar(valor: BigDecimal){
+        //ToDo
+    }
+}
--- src/main/kotlin/one/digitalinnovation/digionebankcourse/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebankcourse/Pessoa.kt
@@ -1,24 +1,8 @@
 package one.digitalinnovation.digionebankcourse
 
-class Pessoa {
-    var nome: String? = null
-    var cpf: String? = null
-
-    //Consigo fazer inner class tbm
-    inner class Endereco{
-        var endereco: String? = null
-    }
-}
-
-
-fun main(){
-    var pessoa = Pessoa()
-    pessoa.nome = "Dhiego"
-    pessoa.cpf="123464"
-    var endereco = pessoa.Endereco()
-    endereco.endereco = "Rua do Teste"
-
-    println(pessoa.nome)
-    println(pessoa.cpf)
-    println(endereco.endereco)
+class Pessoa (
+    var nome:String?,
+    var cpf: String?
+){
+    //ToDo
 }
